5a33adb8c43acfd8798cd94ecd6888b766afa359
Karen Hambro <khambro@gmail.com>
2015-11-04 16:15:48 -0800

    changes almost everything

--- website/app/controllers/home_controller.rb
+++ website/app/controllers/home_controller.rb
@@ -1,7 +1,25 @@
 class HomeController < ApplicationController
 
   def index
-    #just defining it as a valid thing that could use html
+    @about = [{p: "I will be an apprentice at Code Builders full time until Spring, 2016!",
+      photo: "/assets/small_CB.jpg"},
+      {p: "I spent 4 years living in Barcelona and attended graduate school there. I look forward to the day I can make it my second home.", photo: "/assets/barcelona.jpg"},
+      {p:"My family is pretty great. It's made up of one human, James, and two rescue mutts, Wolfie and Beastie.", photo: "/assets/small_lightbground.jpeg"},
+      ]
+      @projects = [{p:"\"Yappy Trails\", a dog vacationing app, allows you to plan vacations with your pooch where she's not only welcome to join, but encouraged!", photo:"/assets/dog.jpeg"}, {p:"For mastering the art of divying up the housework equally, \"Who's taking out the trash?!\", coming Winter, 2015.", photo: "/assets/clean.jpeg"}, {p: "\"Babe, get me a cookie\" allows you to send messages to your partner informing them to bring you a cookie without you even having to speak. They'll even be electronically thanked upon cookie delivery.", photo: "/assets/cookie.jpeg"}]
+
   end
+
+  def resume
+
+  end
+
+  def projects
+
+  end
+
+  def blog
+  end
+
 
 end
--- website/config/routes.rb
+++ website/config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
   get "/", to: "home#index"
 
+  get "/resume", to: "home#resume"
+
+  get "/projects", to: "home#projects"
+
+  get "/blog", to: "home#blog"
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
