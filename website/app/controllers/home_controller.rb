1da015bc2e598142fbced8664aae4bc53e82ffff
Karen Hambro <khambro@gmail.com>
2015-11-10 12:30:29 -0800

    changes absolutely everything

---
+++ website/app/controllers/home_controller.rb
@@ -0,0 +1,30 @@
+class HomeController < ApplicationController
+
+  def index
+    @title="Karen Hambro"
+      @about = [{p: "I will be an apprentice at Code Builders full time until Spring, 2016!",
+      photo: "small_CB.jpg"},
+      {p: "I spent 4 years living in Barcelona and attended graduate school there. I look forward to the day I can make it my second home.", photo: "barcelona.jpg"},
+      {p:"My family is pretty great. It's made up of one human, James, and two rescue mutts, Wolfie and Beastie.", photo: "small_lightbground.jpeg"},
+      ]
+      @projects = [{p:"\"Yappy Trails\", a dog vacationing app, allows you to plan vacations with your pooch where she's not only welcome to join, but encouraged!", photo:"dog.jpeg"},
+      {p:"For mastering the art of divying up the housework equally, \"Who's taking out the trash?!\", coming Winter, 2015.", photo: "clean.jpeg"}, {p: "\"Babe, get me a cookie\" allows you to send messages to your partner informing them to bring you a cookie without you even having to speak. They'll even be electronically thanked upon cookie delivery.", photo: "cookie.jpeg"}]
+
+
+  end
+
+  def resume
+    @title="Hambro Resume"
+  end
+
+  def projects
+    @title="Hambro Projects"
+  end
+
+
+  def blog
+    @title="Hambro Blog"
+  end
+
+
+end
---
+++ website/app/models/fortune_cookie.rb
@@ -0,0 +1,27 @@
+class FortuneCookie
+
+
+  def fortune
+    @fortunes = ["You are worth loving, you are also worth the effort it takes to love you.",
+    "You are thinking about doing something. Don't do it, it won't help anything.",
+    "Happiness is not the absence of conflict, but the ability to cope with it.",
+    "Sometimes you just need to lay on the floor.",
+    "Excuses are easy to manufacture, and hard to sell.",
+    "You may be hungry soon: order takeout now.",
+    "You learn from your mistakes...you will learn a lot today!",
+    "Your worst enemy has a crush on you!",
+    "A good way to keep healthy is to eat more Chinese food.",
+    "I think you ate your fortune while you were eating your cookie!",
+    "Next full moon brings an enchanting evening.",
+    "Tomorrow morning, take a left turn as soon as you leave home.",
+    "Keep it simple. The more you say, the less people remember."
+    
+   ]
+
+    @fortunes.sample
+
+  end
+
+
+
+end
