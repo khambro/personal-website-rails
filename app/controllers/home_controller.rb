12e82a65711e182eb2ad457003ea545445316de1
Karen Hambro <khambro@gmail.com>
2015-11-18 14:17:56 -0800

    file update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
       {p:"My family is pretty great. It's made up of one human, James, and two rescue mutts, Wolfie and Beastie.", photo: "small_lightbground.jpg"},
       ]
       @projects = [{p:"\"Yappy Trails\", a dog vacationing app, allows you to plan vacations with your pooch where she's not only welcome to join, but encouraged!", photo:"dog.jpg"},
-      {p:"For mastering the art of divying up the housework equally, \"Who's taking out the trash?!\", coming Winter, 2015.", photo: "clean.jpeg"}, {p: "\"Babe, get me a cookie\" allows you to send messages to your partner informing them to bring you a cookie without you even having to speak. They'll even be electronically thanked upon cookie delivery.", photo: "cookie.jpg"}]
+      {p:"For mastering the art of divying up the housework equally, \"Who's taking out the trash?!\", coming Winter, 2015.", photo: "clean.jpg"}, {p: "\"Babe, get me a cookie\" allows you to send messages to your partner informing them to bring you a cookie without you even having to speak. They'll even be electronically thanked upon cookie delivery.", photo: "cookie.jpg"}]
 
 
   end
